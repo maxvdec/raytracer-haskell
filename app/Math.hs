a1f84cba3942f908ee6991893b9027e558b837d6
Max Van den Eynde <maxvdec@gmail.com>
2026-07-28 17:34:36 +0200

    Finished math modules

--- app/Math.hs
+++ app/Math.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Math where
 
 type Resolution = (Integer, Integer)
@@ -9,3 +11,68 @@ normalizeColor (r, g, b) = (round (r * 255), round (g * 255), round (b * 255))
 
 ratio :: Integer -> Integer -> Float
 ratio a b = fromIntegral a / ((fromIntegral b) - 1)
+
+-- Vec 3 class
+data Vector3 = Vector3 Float Float Float deriving (Show, Eq)
+
+instance Num Vector3 where
+    (+) :: Vector3 -> Vector3 -> Vector3
+    Vector3 ax ay az + Vector3 bx by bz =
+        Vector3 (ax + bx) (ay + by) (az + bz)
+
+    (-) :: Vector3 -> Vector3 -> Vector3
+    Vector3 ax ay az - Vector3 bx by bz =
+        Vector3 (ax - bx) (ay - by) (az + bz)
+
+    (*) :: Vector3 -> Vector3 -> Vector3
+    Vector3 ax ay az * Vector3 bx by bz =
+        Vector3 (ax * bx) (ay * by) (az * bz)
+
+    abs :: Vector3 -> Vector3
+    abs (Vector3 ax ay az) =
+        Vector3 (abs ax) (abs ay) (abs az)
+
+    signum :: Vector3 -> Vector3
+    signum (Vector3 ax ay az) =
+        Vector3 (signum ax) (signum ay) (signum az)
+
+    fromInteger :: Integer -> Vector3
+    fromInteger n =
+        let value = fromInteger n
+         in Vector3 value value value
+
+(.*) :: Float -> Vector3 -> Vector3
+scalar .* Vector3 x y z =
+    Vector3 (scalar * x) (scalar * y) (scalar * z)
+
+(*.) :: Vector3 -> Float -> Vector3
+Vector3 x y z *. scalar =
+    Vector3 (scalar * x) (scalar * y) (scalar * z)
+
+(./) :: Float -> Vector3 -> Vector3
+scalar ./ Vector3 x y z =
+    Vector3 (scalar / x) (scalar / y) (scalar / z)
+
+(/.) :: Vector3 -> Float -> Vector3
+Vector3 x y z /. scalar =
+    Vector3 (scalar / x) (scalar / y) (scalar / z)
+
+dot :: Vector3 -> Vector3 -> Float
+dot (Vector3 ax ay az) (Vector3 bx by bz) =
+    (ax * bx) + (ay * by) + (az * bz)
+
+cross :: Vector3 -> Vector3 -> Vector3
+cross (Vector3 ax ay az) (Vector3 bx by bz) =
+    let x = ay * bz - az * by
+        y = az * bx - ax * bz
+        z = ax * by - ay * bx
+     in Vector3 x y z
+
+length_squared :: Vector3 -> Float
+length_squared (Vector3 x y z) = (x * x) + (y * y) + (z * z)
+
+vecLength :: Vector3 -> Float
+vecLength vec = sqrt (length_squared vec)
+
+unit :: Vector3 -> Vector3
+unit vec = vec /. (vecLength vec)
