87894edae9f5e579ac10ebd1dead388c5157706c
Max Van den Eynde <maxvdec@gmail.com>
2026-07-31 15:53:33 +0200

    Smooth noise

--- app/Graphics/Noise.hs
+++ app/Graphics/Noise.hs
@@ -9,7 +9,7 @@ import Math (Point3, RandomGenerator, getX, getY, getZ, randomFloat)
 import System.Random (randomRIO)
 
 data Perlin = Perlin
-    { noiseRandfloat :: [Float]
+    { noiseRandfloat :: V.Vector Float
     , permX :: V.Vector Int
     , permY :: V.Vector Int
     , permZ :: V.Vector Int
@@ -33,18 +33,37 @@ makePermutation size =
 
 noise :: Perlin -> Point3 -> Float
 noise perlin p =
-    let elems = length (noiseRandfloat perlin)
-        i = ((floor (4 * getX p)) :: Int) .&. (elems - 1)
-        j = ((floor (4 * getY p)) :: Int) .&. (elems - 1)
-        k = ((floor (4 * getZ p)) :: Int) .&. (elems - 1)
-        noiseX = (permX perlin) V.! i
-        noiseY = (permY perlin) V.! j
-        noiseZ = (permZ perlin) V.! k
-     in (noiseRandfloat perlin) !! (noiseX `xor` noiseY `xor` noiseZ)
+    let u = (getX p) - fromInteger (floor (getX p))
+        v = (getY p) - fromInteger (floor (getY p))
+        w = (getZ p) - fromInteger (floor (getZ p))
+
+        i = floor (getX p) :: Int
+        j = floor (getY p) :: Int
+        k = floor (getZ p) :: Int
+
+        corners = makeCorners i j k
+     in trilinearInterpolation corners u v w
+  where
+    hashCorner :: Int -> Int -> Int -> Int -> Int -> Int -> Corner
+    hashCorner i j k di dj dk =
+        let elems = V.length (noiseRandfloat perlin)
+            wrappedX = (i + di) .&. (elems - 1)
+            wrappedY = (j + dj) .&. (elems - 1)
+            wrappedZ = (k + dk) .&. (elems - 1)
+            permutedX = (permX perlin) V.! wrappedX
+            permutedY = (permY perlin) V.! wrappedY
+            permutedZ = (permZ perlin) V.! wrappedZ
+
+            noiseIndex = permutedX `xor` permutedY `xor` permutedZ
+         in (noiseRandfloat perlin) V.! noiseIndex
+
+    makeCorners :: Int -> Int -> Int -> [Corner]
+    makeCorners i j k =
+        [hashCorner i j k di dj dk | di <- [0, 1], dj <- [0, 1], dk <- [0, 1]]
 
 makePerlinNoise :: RandomGenerator -> Int -> IO Perlin
 makePerlinNoise gen size = do
-    randomNumbers <- replicateM size (randomFloat gen)
+    randomNumbers <- V.replicateM size (randomFloat gen)
     permutationX <- makePermutation size
     permutationY <- makePermutation size
     permutationZ <- makePermutation size
@@ -56,3 +75,27 @@ makePerlinNoise gen size = do
             , permZ = permutationZ
             }
         )
+
+type Corner = Float
+
+trilinearInterpolation :: [Corner] -> Float -> Float -> Float -> Float
+trilinearInterpolation [] _ _ _ = 0
+trilinearInterpolation (corner : corners) u v w =
+    let (x, y, z) = getCornerCoordinatesForIndex (7 - (length corners))
+        weight = getWeightForCorner x y z
+     in (weight * corner) + trilinearInterpolation corners u v w
+  where
+    getWeightForCorner :: Int -> Int -> Int -> Float
+    getWeightForCorner x y z =
+        let weightX = contrib x u
+            weightY = contrib y v
+            weightZ = contrib z w
+         in weightX * weightY * weightZ
+      where
+        contrib :: Int -> Float -> Float
+        contrib n coord =
+            if n == 1 then coord else 1 - coord
+
+    getCornerCoordinatesForIndex :: Int -> (Int, Int, Int)
+    getCornerCoordinatesForIndex i =
+        (i `div` 4, (i `div` 2) `mod` 2, i `mod` 2)
